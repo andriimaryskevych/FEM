750358616f7c534715363e06af492e87f3406577
Andrii Maryskevych <andrii.maryskecych@gmail.com>
2019-05-29 23:30:13 +0300

    Add cubic equation solver

---
+++ FEM/Solvers/Cubic.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Numerics;
+using MathNet.Numerics;
+
+
+namespace FEM.Solvers
+{
+    class Cubuc
+    {
+        public static double[] Solve(double a, double b, double c, double d)
+        {
+            var roots = FindRoots.Cubic(d, c, b, a);
+
+            Complex root1 = roots.Item1;
+            Complex root2 = roots.Item2;
+            Complex root3 = roots.Item3;
+
+            double[] res = new double[3] {
+                root1.Real,
+                root2.Real,
+                root3.Real
+            };
+
+            return res;
+        }
+    }
+}
