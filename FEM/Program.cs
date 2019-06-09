8aa31b4197f35810c0fd4558feb4353b35d706e0
Andrii Maryskevych <andrii.maryskecych@gmail.com>
2019-06-09 09:45:31 +0300

    Logging parameters

--- FEM/Program.cs
+++ FEM/Program.cs
@@ -34,6 +34,21 @@ namespace FEM
                 Environment.Exit(0);
             }
 
+            Console.WriteLine("parameters.sizeX: {0}", parameters.sizeX);
+            Console.WriteLine("parameters.sizeY: {0}", parameters.sizeY);
+            Console.WriteLine("parameters.sizeZ: {0}", parameters.sizeZ);
+
+            Console.WriteLine("parameters.xAxisFEMCount: {0}", parameters.xAxisFEMCount);
+            Console.WriteLine("parameters.yAxisFEMCount: {0}", parameters.yAxisFEMCount);
+            Console.WriteLine("parameters.zAxisFEMCount: {0}", parameters.zAxisFEMCount);
+
+            Console.WriteLine("parameters.puasson: {0}", parameters.puasson);
+            Console.WriteLine("parameters.young: {0}", parameters.young);
+
+            foreach (Pressure a in parameters.load) {
+                Console.WriteLine("fe: {0}, part: {1}, load: {2}", a.fe, a.part, a.pressure);
+            }
+
             new FiniteElementMethod(parameters, mesh).Start();
         }
     }
